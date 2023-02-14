00b3b7c2133545e8bbd48e280dc22e56b4ffcd57
Harold <haroldrex12331@gmail.com>
2023-02-14 23:41:35 +0800

    video platforms

--- src/pages/Iam.tsx
+++ src/pages/Iam.tsx
@@ -1,23 +1,59 @@
 import React from "react";
 import styled from "@emotion/styled";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import { FaTiktok } from "react-icons/fa";
 
 const Iam = ({ isDark }: { isDark: boolean }) => {
   return (
-    <MainContainer
-      style={isDark ? { color: "white" } : { color: "var(--dark-background)" }}
-    >
-      <IamContainer className="Iam-container">
-        <section className="upper-section">
-          <p>I AM</p>
-        </section>
-        <h1 className="Iam-haroldP">Harold P.</h1>
-        <section className="lower-section">
-          <p>SPOKEN ARTIST</p>
-        </section>
-      </IamContainer>
-    </MainContainer>
+    <>
+      <MainContainer
+        style={
+          isDark ? { color: "white" } : { color: "var(--dark-background)" }
+        }
+      >
+        <IamContainer className="Iam-container">
+          <section className="upper-section">
+            <p>I AM</p>
+          </section>
+          <h1 className="Iam-haroldP">Harold P.</h1>
+          <section className="lower-section">
+            <p>SPOKEN ARTIST</p>
+          </section>
+        </IamContainer>
+      </MainContainer>
+      <VideoPlat>
+        <VideoPlatIcons>
+          <YouTubeIcon style={{ fontSize: "3rem" }} />
+        </VideoPlatIcons>
+        <VideoPlatIcons>
+          <FaTiktok />
+        </VideoPlatIcons>
+      </VideoPlat>
+    </>
   );
 };
+
+const VideoPlatIcons = styled.div`
+  background-color: #455163;
+  border-radius: 50%;
+  height: 4rem;
+  width: 4rem;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 2rem;
+`;
+
+const VideoPlat = styled.div`
+  margin: 2rem 0 0 0;
+  width: 10%;
+  height: 11rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
 
 const IamContainer = styled.div`
   width: 80%;
@@ -26,7 +62,8 @@ const IamContainer = styled.div`
 const MainContainer = styled.div`
   color: white;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
 
   & .lower-section {
     display: flex;
