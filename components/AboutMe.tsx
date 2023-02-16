c8c5b61585035d350b3ecf0c6b99e1a9dd9af2e1
Harold <haroldrex12331@gmail.com>
2023-02-17 00:28:51 +0800

    other skills

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -5,6 +5,8 @@ import { Slide } from "react-awesome-reveal";
 import { keyframes } from "@emotion/react";
 import Reveal from "react-awesome-reveal";
 import Grid from "@mui/material/Grid";
+import { FaGuitar, FaDrum, FaMusic } from "react-icons/fa";
+import { BsCalendarFill } from "@react-icons/all-files/Bs/BsCalendarFill";
 
 import { ThemeProps } from "types/types";
 
@@ -30,8 +32,21 @@ const customAnimationRight = keyframes`
   }
 `;
 
+const customAnimationUp = keyframes`
+  from {
+    opacity: 0;
+    transform: translate3d(0, 50px, 0);
+  }
+  to {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+`;
+
 const AboutMe = ({ isDark }: ThemeProps) => {
   const [isActive, setIsActive] = useState(true);
+  const [otherSkills, setOtherSkills] = useState(false);
+
   return (
     <MainContainer
       style={isDark ? { color: "white" } : { color: "var(--dark-background)" }}
@@ -50,7 +65,10 @@ const AboutMe = ({ isDark }: ThemeProps) => {
                   ? "var(--purple)"
                   : "var(--darker-color)",
               }}
-              onClick={() => setIsActive(true)}
+              onClick={() => {
+                setIsActive(true);
+                setOtherSkills(false);
+              }}
             >
               Know me more
             </Button>
@@ -62,57 +80,197 @@ const AboutMe = ({ isDark }: ThemeProps) => {
                   ? "var(--darker-color)"
                   : "var(--purple)",
               }}
-              onClick={() => setIsActive(false)}
+              onClick={() => {
+                setIsActive(false);
+                setOtherSkills(true);
+              }}
             >
               Other Skills
             </Button>
           </TwinButton>
         </Fade>
-        <Grid container>
-          <Grid item md={6} xs={12}>
-            <Reveal
-              keyframes={customAnimation}
-              duration={500}
-              triggerOnce={true}
-            >
-              <Fade duration={1000} triggerOnce={true}>
-                <Image src="normal.png" />
-              </Fade>
-            </Reveal>
+        {!otherSkills ? (
+          <Grid container>
+            <Grid item md={6} xs={12}>
+              <Reveal
+                keyframes={customAnimation}
+                duration={500}
+                triggerOnce={true}
+              >
+                <Fade duration={1000} triggerOnce={true}>
+                  <Image src="normal.png" />
+                </Fade>
+              </Reveal>
+            </Grid>
+            <Grid item md={6} xs={12}>
+              <Reveal keyframes={customAnimationRight} triggerOnce={true}>
+                <Fade duration={1000} triggerOnce={true}>
+                  <AboutMeContent>
+                    <h1>Spoken word poet | Fullstack developer</h1>
+                    <p>
+                      Greetings, I'm Harold, a spoken word poet and fullstack
+                      web developer who believes that art and technology can
+                      coexist in perfect harmony. As a poet, I excel at crafting
+                      compelling stories and conveying powerful emotions through
+                      carefully chosen words. With my skillful use of language
+                      and keen sense of rhythm, I can capture audiences and hold
+                      them spellbound. I bring this same level of artistry to my
+                      work as a fullstack web developer, where I use my
+                      expertise to design and build websites and web
+                      applications that are both aesthetically pleasing and
+                      highly functional. My attention to detail and commitment
+                      to delivering quality results enable me to consistently
+                      exceed client expectations. So, whether I'm performing on
+                      stage or in front of my computer, I'm always striving to
+                      find the perfect balance between my creative and logical
+                      sides.
+                    </p>
+                  </AboutMeContent>
+                </Fade>
+              </Reveal>
+            </Grid>
           </Grid>
-          <Grid item md={6} xs={12}>
-            <Reveal keyframes={customAnimationRight} triggerOnce={true}>
-              <Fade duration={1000} triggerOnce={true}>
-                <AboutMeContent>
-                  <h1>Spoken word poet | Fullstack developer</h1>
-                  <p>
-                    Greetings, I'm Harold, a spoken word poet and fullstack web
-                    developer who believes that art and technology can coexist
-                    in perfect harmony. As a poet, I excel at crafting
-                    compelling stories and conveying powerful emotions through
-                    carefully chosen words. With my skillful use of language and
-                    keen sense of rhythm, I can capture audiences and hold them
-                    spellbound. I bring this same level of artistry to my work
-                    as a fullstack web developer, where I use my expertise to
-                    design and build websites and web applications that are both
-                    aesthetically pleasing and highly functional. My attention
-                    to detail and commitment to delivering quality results
-                    enable me to consistently exceed client expectations. So,
-                    whether I'm performing on stage or in front of my computer,
-                    I'm always striving to find the perfect balance between my
-                    creative and logical sides.
-                  </p>
-                </AboutMeContent>
-              </Fade>
-            </Reveal>
-          </Grid>
-        </Grid>
+        ) : (
+          <Fade duration={1000}>
+            <OtherSkillsContainer>
+              <Grid container>
+                <Grid item xs={12} md={6} className="center-grid">
+                  <Reveal keyframes={customAnimationUp} triggerOnce={true}>
+                    <LargeCard>
+                      <h2>Drumming</h2>
+                      <p>
+                        <FaDrum style={{ margin: "0 5px 0 0 " }} /> I've been
+                        playing drums since 2020
+                      </p>
+                      <p>
+                        {" "}
+                        <BsCalendarFill style={{ margin: "0 5px 0 0 " }} /> This
+                        video is from 2022
+                      </p>
+                      <div
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "center",
+                          margin: "20px 0 1rem 0",
+                        }}
+                      >
+                        <hr
+                          style={{
+                            width: "100%",
+                            border: "1px solid #6a6e75",
+                            marginRight: "10px",
+                          }}
+                        />
+                        <h2 style={{ fontSize: "24px", fontWeight: "bold" }}>
+                          <FaMusic />
+                        </h2>
+                        <hr
+                          style={{
+                            width: "100%",
+                            border: "1px solid #6a6e75",
+                            marginLeft: "10px",
+                          }}
+                        />
+                      </div>
+                      <GIF src="dec 31, 2022 - Trim.gif" />
+                    </LargeCard>
+                  </Reveal>
+                </Grid>
+                <Grid item xs={12} md={6} className="center-grid">
+                  <Reveal
+                    keyframes={customAnimationUp}
+                    delay={500}
+                    triggerOnce={true}
+                  >
+                    <LargeCard>
+                      <h2>Playing Guitar</h2>
+                      <div>
+                        <p>
+                          <FaGuitar style={{ margin: "0 5px 0 0 " }} /> I've
+                          been playing guitar since 2016
+                        </p>
+                        <p>
+                          {" "}
+                          <BsCalendarFill
+                            style={{ margin: "0 5px 0 0 " }}
+                          />{" "}
+                          This video is from 2018
+                        </p>
+                      </div>
+                      <div
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "center",
+                          margin: "20px 0 1rem 0",
+                        }}
+                      >
+                        <hr
+                          style={{
+                            width: "100%",
+                            border: "1px solid #6a6e75",
+                            marginRight: "10px",
+                          }}
+                        />
+                        <h2 style={{ fontSize: "24px", fontWeight: "bold" }}>
+                          <FaMusic />
+                        </h2>
+                        <hr
+                          style={{
+                            width: "100%",
+                            border: "1px solid #6a6e75",
+                            marginLeft: "10px",
+                          }}
+                        />
+                      </div>
+                      <GIF src="Harold - Trim - Trim.gif" />
+                    </LargeCard>
+                  </Reveal>
+                </Grid>
+              </Grid>
+            </OtherSkillsContainer>
+          </Fade>
+        )}
       </AboutMeContentContainer>
     </MainContainer>
   );
 };
 
-const Section = styled.div``;
+const GIF = styled.img`
+  width: 100%;
+  border-radius: 10px;
+`;
+
+const Divider = styled.div`
+  border-bottom: 1px solid white;
+  width: 100%;
+`;
+
+const LargeCard = styled.div`
+  & .lower-hobby-desc {
+    margin: 1rem 0 2rem 0;
+  }
+  & h2 {
+    letter-spacing: 3px;
+  }
+  & p {
+    color: #6f7175;
+    margin-top: 5px;
+  }
+  background-color: var(--darker-color);
+  min-height: 25rem;
+  width: 80%;
+  min-height: 28rem;
+  border-radius: 10px;
+  padding: 5rem 3rem 0 3rem;
+  font-family: var(--formal-font);
+  margin-top: 2rem;
+`;
+
+const OtherSkillsContainer = styled.div`
+  margin: 5rem 0 0 0;
+`;
 
 const AboutMeContent = styled.div`
   & h1 {
