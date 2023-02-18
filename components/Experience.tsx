dc191de0ca183e6019a4d428eb0ca2c866e9edb3
Harold <haroldrex12331@gmail.com>
2023-02-19 00:42:50 +0800

    projects map

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -5,63 +5,100 @@ import YouTubeIcon from "@mui/icons-material/YouTube";
 
 import { ThemeProps } from "types/types";
 
+const projectsList = [
+  {
+    id: "01",
+    title: "Jesus reigns 2022",
+    desc: " It is a time to bring honor and praise by testifying the Lord's powerful work in their province and give Glory to God.",
+    image: "Jesus-reigns.jpg",
+  },
+  {
+    id: "02",
+    title: "Mindanao youth Congress 2022",
+    desc: "The Mindanao Youth Congress is a gathering of young people from across the region, brought together by a shared passion for serving the Lord and making a positive impact in their communities.",
+    image: "myc.jpg",
+  },
+  {
+    id: "03",
+    title: "El and Abi's wedding",
+    desc: "The Mindanao Youth Congress is a gathering of young people from across the region, brought together by a shared passion for serving the Lord and making a positive impact in their communities.",
+    image: "wedding1.jpg",
+  },
+  {
+    id: "04",
+    title: "NCMDC representative for national poetry",
+    desc: "The Mindanao Youth Congress is a gathering of young people from across the region, brought together by a shared passion for serving the Lord and making a positive impact in their communities.",
+    image: "ncmdc1.png",
+  },
+  {
+    id: "05",
+    title: "Mothers day entry",
+    desc: "The Mindanao Youth Congress is a gathering of young people from across the region, brought together by a shared passion for serving the Lord and making a positive impact in their communities.",
+    image: "mothersday.png",
+  },
+];
+
 const Experience = ({ isDark }: ThemeProps) => {
   return (
     <MainContainer
       style={isDark ? { color: "white" } : { color: "var(--dark-background)" }}
     >
       <ExperienceText>PROJECTS</ExperienceText>
-      <h1>01</h1>
-      <PerProjects>
-        <Grid container>
-          <Grid item md={6} xs={12}>
-            <PerImage>
-              <Image src="Jesus-reigns.jpg" />
-            </PerImage>
-          </Grid>
-          <Grid item md={6} xs={12}>
-            <PerInfo className="exp-perinfo">
-              <h2>Jesus reigns 2022</h2>
-              <p>
-                It is a time to bring honor and praise by testifying the Lord's
-                powerful work in their province and give Glory to God.
-              </p>
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  margin: "20px 0 1rem 0",
-                }}
-              >
-                <hr
-                  style={{
-                    width: "100%",
-                    border: "1px solid #6a6e75",
-                    marginRight: "10px",
-                  }}
-                />
-                <h2 style={{ fontSize: "24px", fontWeight: "bold" }}>view</h2>
-                <hr
-                  style={{
-                    width: "100%",
-                    border: "1px solid #6a6e75",
-                    marginLeft: "10px",
-                  }}
-                />
-              </div>
-              <div className="icons-section">
-                <YouTubeIcon
-                  style={{
-                    fontSize: "3rem",
-                    color: isDark ? "white" : "black",
-                  }}
-                />
-              </div>
-            </PerInfo>
-          </Grid>
-        </Grid>
-      </PerProjects>
+      {projectsList.map((item) => (
+        <>
+          <h1>{item.id}</h1>
+          <PerProjects>
+            <Grid container>
+              <Grid item md={6} xs={12}>
+                <PerImage>
+                  <Image src={item.image} />
+                </PerImage>
+              </Grid>
+              <Grid item md={6} xs={12}>
+                <PerInfo className="exp-perinfo">
+                  <h2>{item.title}</h2>
+                  <p>{item.desc}</p>
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      margin: "20px 0 1rem 0",
+                    }}
+                  >
+                    <hr
+                      style={{
+                        width: "100%",
+                        border: "1px solid #6a6e75",
+                        marginRight: "10px",
+                      }}
+                    />
+                    <h2 style={{ fontSize: "24px", fontWeight: "bold" }}>
+                      view
+                    </h2>
+                    <hr
+                      style={{
+                        width: "100%",
+                        border: "1px solid #6a6e75",
+                        marginLeft: "10px",
+                      }}
+                    />
+                  </div>
+                  <div className="icons-section">
+                    <YouTubeIcon
+                      style={{
+                        fontSize: "3rem",
+                        color: isDark ? "white" : "black",
+                      }}
+                    />
+                    {/* youtube ? kana dayun */}
+                  </div>
+                </PerInfo>
+              </Grid>
+            </Grid>
+          </PerProjects>
+        </>
+      ))}
     </MainContainer>
   );
 };
